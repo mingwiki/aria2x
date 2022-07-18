5c98cfe33700fe908e578766a2c3a96ff0ea1796
mingwiki <mingwiki@gmail.com>
2022-07-18 18:02:41 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require('express')
 const http = require('http')
+const express = require('express')
 const { Server } = require('socket.io')
+
 const app = express()
 const server = http.createServer(app)
+server.listen(5000, () => {
+  console.log(`listening on http://localhost:5000`)
+})
+
 const io = new Server(server)
 
 app.get('/', (req, res) => {
+  console.log(__dirname)
   res.sendFile(__dirname + '/index.html')
 })
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  console.log('user connected')
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg)
   })
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
-})
-server.listen(5000, () => {
-  console.log('listening on *:5000')
 })
